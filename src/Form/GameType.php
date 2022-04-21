52bfd658b52e908a307d5192c268a57bef69da0e
JPG1993 <jpjosephgarboud@gmail.com>
2022-04-21 16:36:36 +0200

    commit 3 jp

--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Game;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,11 @@ class GameType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('category')
-            ->add('date')
-            ->add('prix')
-            ->add('stock')
+            ->add('name', TextType::class)
+            ->add('category',TextType::class )
+            ->add('date', DateType::class)
+            ->add('prix', TextType::class)
+            ->add('stock', IntegerType::class)
         ;
     }
 
